e67509fa4fffe7b2b775e34a53c62e1b406f8682
saimiiqbal7 <saimiiqbal7@gmail.com>
2025-06-29 20:14:58 +0500

    daily exercises

--- index.py
+++ index.py
@@ -1,68 +1,20 @@
 
-
-"""sally = "sally sells sea shells by the sea shore"
-characters = {}
-best_char = None
-freq = 0
-
-for c in sally:
-    if c in characters:
-        characters[c] = characters[c] +1
-    else:
-        characters[c] = 1
-
-for key in characters.keys():
-    if characters[key] > freq:
-        freq = characters[key]
-        best_char = key
+def checkingIfIn(key:str,
+                 direction:bool=True,
+                 d:dict | None=None) -> bool:
     
+    if d is None:
+        d = {
+            'apple': 2,
+            'pear': 1,
+            'fruit': 19,
+            'orange': 5,
+            'banana': 3,
+            'grapes': 2,
+            'watermelon': 7
+        }
     
-
-print(characters)
-print(best_char)"""
-
-
-"""
-str1 = "I wish I wish with all my heart to fly with dragons in a land apart"
-
-strArr = str1.split()
-
-freq_words = {}
-
-for word in strArr:
-    if word in freq_words:
-        freq_words[word] =freq_words[word]+1
+    if direction:
+        return key in d
     else:
-        freq_words[word] = 1
-
-print(freq_words)"""
-
-
-"""str1 = "peter piper picked a peck of pickled peppers"
-freq = {}
-
-for char in str1:
-
-    if char in freq:
-        freq[char] = freq[char]+1
-    else:
-        freq[char] = 1
-
-
-print(freq)
-"""
-
-
-"""Junior = {
-    "SI 206": 4,
-    "SI 310": 4,
-    "BL 300": 3,
-    "TO 313": 3,
-    "BCOM 350": 1,
-    "MO 300": 3,
-}
-credits = 0
-for value in Junior.values():
-    credits += value
-
-print(credits)"""
+        return key not in d
