4d4a1e919408e4a86c01f46cbf9eb4d276a29086
saimiiqbal7 <saimiiqbal7@gmail.com>
2025-07-06 21:19:10 +0500

    new

--- index.py
+++ index.py
@@ -1,30 +1,31 @@
 
-
-class Person:
-
-    total = 0
-
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        Person.total += 1
-
-    def hi(self):
-        return("hello")
-
-class Student(Person):
-
-    def __init__(self, name, age):
-        Person.__init__(self,name,age)
-    
+import time
+import random
 
 
-saim = Person("saim", 1997)
-obaid = Student("obaid", 1999)
+def getNumberBetween(prompt, min, max):
+    userinp = input(prompt) # ask the first time
 
-print(obaid.hi())
+    while True:
+        try:
+            n = int(userinp) # try casting to an integer
+            if n < min:
+                errmessage = 'Must be at least {}'.format(min)
+            elif n > max:
+                errmessage = 'Must be at most {}'.format(max)
+            else:
+                return n
+        except ValueError: # The user didn't enter a number
+            errmessage = '{} is not a number.'.format(userinp)
 
+        # If we haven't gotten a number yet, add the error message
+        # and ask again
+        userinp = input('{}\n{}'.format(errmessage, prompt))
 
+# Spins the wheel of fortune wheel to give a random prize
+# Examples:
+#    { "type": "cash", "text": "$950", "value": 950, "prize": "A trip to Ann Arbor!" },
+#    { "type": "bankrupt", "text": "Bankrupt", "prize": false },
+#    { "type": "loseturn", "text": "Lose a turn", "prize": false }
 
-        
-        
+getNumberBetween("enter a number", 1, 10)
