a6e71fe6634b365a57cf9c100824a1bfd809dfa9
saimiiqbal7 <saimiiqbal7@gmail.com>
2025-07-15 11:39:55 +0500

    new

--- index.py
+++ index.py
@@ -1,31 +1,11 @@
+# Write your code here.
+def get_args_and_kwargs(*args, **kwargs):
+    
+    if 'num' in kwargs:
+        print('ayo')
+    print((kwargs['num']))
+    print(kwargs)
+    print(args)
 
-import time
-import random
-
-
-def getNumberBetween(prompt, min, max):
-    userinp = input(prompt) # ask the first time
-
-    while True:
-        try:
-            n = int(userinp) # try casting to an integer
-            if n < min:
-                errmessage = 'Must be at least {}'.format(min)
-            elif n > max:
-                errmessage = 'Must be at most {}'.format(max)
-            else:
-                return n
-        except ValueError: # The user didn't enter a number
-            errmessage = '{} is not a number.'.format(userinp)
-
-        # If we haven't gotten a number yet, add the error message
-        # and ask again
-        userinp = input('{}\n{}'.format(errmessage, prompt))
-
-# Spins the wheel of fortune wheel to give a random prize
-# Examples:
-#    { "type": "cash", "text": "$950", "value": 950, "prize": "A trip to Ann Arbor!" },
-#    { "type": "bankrupt", "text": "Bankrupt", "prize": false },
-#    { "type": "loseturn", "text": "Lose a turn", "prize": false }
-
-getNumberBetween("enter a number", 1, 10)
+data = {'hello':'world', 'daddy':'come'}
+get_args_and_kwargs(1,2,3,num=4)
